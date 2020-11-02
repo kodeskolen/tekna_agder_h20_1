47747c3f1c4f4dbddbe639b8adb823ae4181a33e
Marie Roald <roald.marie@gmail.com>
2020-11-02 19:07:35 +0100

    La til strenger.py

---
+++ dag2/live_koding/strenger.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  2 18:30:22 2020
+
+@author: Marie
+"""
+"""
+setning = "Her er en setning"
+for bokstav in setning:
+    print(bokstav)
+"""
+    
+navneliste = ["Eina", "Eli", "Hanna"]
+for navn in navneliste:
+    print(navn[0])
